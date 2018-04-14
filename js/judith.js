7c19359c9a44d0c32d172ab8ad2b5484c9c0770f
Godson C. Jnr <godsoncj.dc@fptb.edu.ng>
2018-04-14 15:28:40 +0100

    Trying to insert contacts into table

--- js/judith.js
+++ js/judith.js
@@ -1,2 +1,13 @@
-
- 
+function myFunction() {
+    var fname = document.getElementById("fname").value;
+    var mname = document.getElementById("mname").value;
+    var lname = document.getElementById("lname").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
+   
+    document.getElementById("tname").innerHTML = fname + ' ' + mname + ' ' + lname;
+    document.getElementById("temail").innerHTML = email;
+    document.getElementById("tphone").innerHTML = phone;
+    document.getElementById("taddress").innerHTML = address; 
+}
